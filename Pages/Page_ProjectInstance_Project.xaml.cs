c053c4fa4d1a077d95b3b1c094cebc947d855189
TideWind <1724285787@qq.com>
2021-10-15 00:05:45 +0800

    实现了文件上传和删除功能

--- Pages/Page_ProjectInstance_Project.xaml.cs
+++ Pages/Page_ProjectInstance_Project.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Co_work.Windows;
+using FTPHelper;
 
 namespace Co_work.Pages
 {
@@ -24,11 +25,12 @@ namespace Co_work.Pages
         public Page_ProjectInstance_Project()
         {
             InitializeComponent();
+            Lv_File.ContextMenu = MenuListView();
         }
 
         public Page_ProjectInstance Owner;
 
-        
+        public FtpHelper ftpHelper = new FtpHelper("103.193.189.241", "Administrator", "adxq@9139");
 
         class FileItem
         { 
@@ -45,14 +47,35 @@ namespace Co_work.Pages
             {
                 string fileAddress = dialog.FileName;
                 string fileName = System.IO.Path.GetFileName(fileAddress);
-                FileItem file = new FileItem { name = fileName };
-                ListViewItem item = new ListViewItem();
-                item.Content = file;
-                Lv_File.Items.Add(item);
-                item.MouseRightButtonUp += ListViewItem_MouseRightButtonUp;
-                item.ContextMenu = MenuFile();
+               
+                FileUploadAsync(fileAddress, fileName);
             }
+            
         }
+
+        private async Task FileUploadAsync(string fileAddress, string fileName)
+        {
+            await Task.Run(() => FileUpload(fileAddress, fileName));
+            FileItemCreate(fileName);
+        }
+
+        private void FileUpload(string fileAddress, string fileName)
+        {
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(fileAddress);
+            ftpHelper.Upload(fileInfo, "/Test/" + fileName);
+        }
+
+        private void FileItemCreate(string fileName)
+        {
+            FileItem file = new FileItem { name = fileName };
+            ListViewItem item = new ListViewItem();
+            item.Content = file;
+            item.DataContext = file;
+            Lv_File.Items.Add(item);
+            item.MouseRightButtonUp += ListViewItem_MouseRightButtonUp;
+            item.ContextMenu = MenuFile();
+        }
+
 
         void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -72,7 +95,7 @@ namespace Co_work.Pages
 
         private void DeleteFile_Click(object sender, RoutedEventArgs e)
         {
-
+            ftpHelper.Delete("/Test/" + ((Lv_File.SelectedItem as ListViewItem).DataContext as FileItem).name);
         }
 
         private void DeleteFolder_Click(object sender, RoutedEventArgs e)
@@ -81,6 +104,11 @@ namespace Co_work.Pages
         }
 
         private void Download_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
         {
 
         }
@@ -130,6 +158,22 @@ namespace Co_work.Pages
             itemDelete.Click += DeleteFolder_Click;
             menu.Items.Add(itemRename);
             menu.Items.Add(itemDelete);
+
+            return menu;
+        }
+
+        private ContextMenu MenuListView()
+        {
+            ContextMenu menu = new ContextMenu();
+            MenuItem itemRefresh = new MenuItem { Name = "Refresh", Header = "刷新" };
+            itemRefresh.Click += Refresh_Click;
+            MenuItem itemUpload = new MenuItem { Name = "Upload", Header = "上传文件" };
+            itemUpload.Click += Btn_Upload_Click;
+            MenuItem itemNewFolder = new MenuItem { Name = "NewFolder", Header = "新建文件夹" };
+            itemNewFolder.Click += Btn_NewFolder_Click;
+            menu.Items.Add(itemRefresh);
+            menu.Items.Add(itemUpload);
+            menu.Items.Add(itemNewFolder);
 
             return menu;
         }
